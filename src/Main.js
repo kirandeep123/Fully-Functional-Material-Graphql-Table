f702559b1eabae506fb57e2caa0fe13e1201cbfd
kirandeep123 <me@kiran.cc>
2019-08-23 21:31:57 +0530

    keyword change

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,37 @@
 import React from "react";
 import MaterialTable from "material-table";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import ListItemText from "@material-ui/core/ListItemText";
+import IconButton from "@material-ui/core/IconButton";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import DeleteIcon from "@material-ui/icons/Delete";
+import { textAlign } from "@material-ui/system";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+    textAlign:'center'
+  }
+}));
 
 export default function Main() {
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
   const [state, setState] = React.useState({
     columns: [{ title: "Category", field: "category" }],
     data: [
@@ -12,35 +42,49 @@ export default function Main() {
     ]
   });
 
+  function generate(element) {
+    return [0, 1, 2].map(value =>
+      React.cloneElement(element, {
+        key: value
+      })
+    );
+  }
+
   return (
     <MaterialTable
       title="Category"
       columns={state.columns}
       data={state.data}
       detailPanel={rowData => {
-        return <MaterialTable 
-        title="KeyWords"
-        columns={state.columns}
-        editable={{ onRowDelete: oldData =>
-        new Promise(resolve => {
-          setTimeout(() => {
-            resolve();
-            const data = [...state.data];
-            data.splice(data.indexOf(oldData), 1);
-            setState({ ...state, data });
-          }, 600);
-        }),
-        onRowAdd: newData =>
-          new Promise(resolve => {
-            setTimeout(() => {
-              resolve();
-              const data = [...state.data];
-              data.push(newData);
-              setState({ ...state, data });
-            }, 600);
-          }),
-      }}
-        />;
+        return (
+          <Grid item xs={12} md={6}>
+            <Typography variant="h6" className={classes.title}>
+              Keywords
+            </Typography>
+            <div className={classes.demo}>
+              <List dense={dense}>
+                {generate(
+                  <ListItem>
+                    <ListItemAvatar>
+                      {/* Add Keywords Functionality here */}
+                    </ListItemAvatar>
+                    <ListItemText
+                      primary="Single-line item"
+                      secondary={secondary ? "Secondary text" : null}
+                    />
+                    
+                    {/* Delete Keyword functionality */ }
+                    <ListItemSecondaryAction>
+                      <IconButton edge="end" aria-label="delete">
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItemSecondaryAction>
+                  </ListItem>
+                )}
+              </List>
+            </div>
+          </Grid>
+        );
       }}
       editable={{
         onRowAdd: newData =>
